c703b00f9632e41ec91d7c289225b15bf0487ebf
David Padilla <david@easybroker.com>
2015-02-24 01:02:44 -0600

    Finishing touches to final design

--- app/controllers/crowdblog/posts_controller.rb
+++ app/controllers/crowdblog/posts_controller.rb
@@ -1,5 +1,9 @@
 class Crowdblog::PostsController < ApplicationController
   def index
-    @posts = Crowdblog::Post.all
+    @posts = Crowdblog::Post.published
+  end
+
+  def show
+    @post = Crowdblog::Post.where(permalink: params[:id]).first
   end
 end
