5440a312225c201533f9328eb3dccdd584c203bb
David Padilla <david@easybroker.com>
2015-02-24 01:19:43 -0600

    Prepare navigation even more

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   mount Crowdblog::Engine => '/'
 
+  get '/:id', to: 'crowdblog/posts#show', defaults: { id: 'about-me' }, as: 'about_me'
+
   resources :posts, only: [:show], controller: 'crowdblog/posts'
 end
