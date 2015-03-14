96d284f5e4c88cabcd32ef14f1d726b09c8d9ea7
David Padilla <david@easybroker.com>
2015-03-14 14:57:41 -0600

    Fix index page post order

--- app/controllers/crowdblog/posts_controller.rb
+++ app/controllers/crowdblog/posts_controller.rb
@@ -1,6 +1,6 @@
 class Crowdblog::PostsController < ApplicationController
   def index
-    @posts = Crowdblog::Post.published.where(cms: false)
+    @posts = Crowdblog::Post.published.where(cms: false).order("published_at DESC")
   end
 
   def show
