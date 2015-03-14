839f3c2401795f2c556a7b0a6ec19201d3c221a6
David Padilla <david@easybroker.com>
2015-03-14 13:52:51 -0600

    Fix the root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount Crowdblog::Engine => '/'
 
-  get '/:id', to: 'crowdblog/posts#show', defaults: { id: 'about-me' }, as: 'about_me'
+  get '/about-me', to: 'crowdblog/posts#show', defaults: { id: 'about-me' },
+    as: 'about_me'
 
   resources :posts, only: [:show], controller: 'crowdblog/posts'
 end
