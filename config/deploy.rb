3bb9cea1de464d4bf1962ea4fd55f57525ba9bff
David Padilla <david@easybroker.com>
2015-03-21 19:13:49 -0600

    Remove unusued deploy line

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 set :assets_roles, [:app]
 set :conditionally_migrate, true           # Defaults to false. If true, it's skip migration if files in db/migrate not modified
-set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
 
 namespace :deploy do
 
