df43ad0898a67ac492286a0d265abb857d9c20c9
David Padilla <david@easybroker.com>
2015-03-27 09:27:49 -0600

    Add more posts section

--- app/controllers/crowdblog/posts_controller.rb
+++ app/controllers/crowdblog/posts_controller.rb
@@ -5,6 +5,14 @@ class Crowdblog::PostsController < ApplicationController
   end
 
   def show
-    @post = ::Post.where(id: params[:id]).first || ::Post.where(permalink: params[:id]).first
+    @post = ::Post.where(id: params[:id]).where(cms: false).first || ::Post.where(permalink: params[:id]).where(cms: false).first
+    if @post
+      @posts = []
+      @posts << Post.published_and_ordered.where(cms: false).where("published_at < ?", @post.published_at).limit(2)
+      @posts << Post.published_and_ordered.where(cms: false).where("published_at > ?", @post.published_at).limit(2)
+      @posts = @posts.select { |p| !p.nil? }.flatten
+    else
+      render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
+    end
   end
 end
