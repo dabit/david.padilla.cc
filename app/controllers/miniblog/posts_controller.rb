0df703a8da9c3e029b2b4c15e0cfdbbdd674402e
David Padilla <david@easybroker.com>
2015-03-29 22:17:19 -0600

    Move the post code to a decorator

--- app/controllers/miniblog/posts_controller.rb
+++ app/controllers/miniblog/posts_controller.rb
@@ -2,19 +2,19 @@ class Miniblog::PostsController < ApplicationController
   before_filter :set_cache_control_headers
 
   def index
-    @posts = ::Post.published_and_ordered.where(cms: false).limit(6)
+    @posts = Miniblog::Post.published_and_ordered.where(cms: false).limit(6)
     @post = @posts.to_a.shift
     @push_state = main_app.post_path(@post)
-    set_surrogate_key_header Post.table_key
+    set_surrogate_key_header Miniblog::Post.table_key
   end
 
   def show
-    @post = ::Post.where(id: params[:id]).where(cms: false).first || ::Post.where(permalink: params[:id]).where(cms: false).first
+    @post = Miniblog::Post.where(id: params[:id]).where(cms: false).first || ::Post.where(permalink: params[:id]).where(cms: false).first
     if @post
       set_surrogate_key_header @post.record_key
       @posts = []
-      @posts << Post.published_and_ordered.where(cms: false).where("published_at < ?", @post.published_at).limit(2)
-      @posts << Post.published_and_ordered.where(cms: false).where("published_at > ?", @post.published_at).limit(2)
+      @posts << Miniblog::Post.published_and_ordered.where(cms: false).where("published_at < ?", @post.published_at).limit(2)
+      @posts << Miniblog::Post.published_and_ordered.where(cms: false).where("published_at > ?", @post.published_at).limit(2)
       @posts = @posts.select { |p| !p.nil? }.flatten
     else
       render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
--- app/decorators/models/miniblog/posts_decorator.rb
+++ app/decorators/models/miniblog/posts_decorator.rb
@@ -1,4 +1,4 @@
-class Post < Miniblog::Post
+Miniblog::Post.class_eval do
   belongs_to :category
 
   delegate :name, to: :category, prefix: true
@@ -19,3 +19,4 @@ class Post < Miniblog::Post
     end
   end
 end
+
