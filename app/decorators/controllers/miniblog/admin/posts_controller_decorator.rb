79ffa730af67cd3c208f9cc44bd67ccadd8890ff
David Padilla <david@easybroker.com>
2015-04-14 14:25:06 -0500

    Change the way posts are purged on fastly to see if it actually works now

--- app/decorators/controllers/miniblog/admin/posts_controller_decorator.rb
+++ app/decorators/controllers/miniblog/admin/posts_controller_decorator.rb
@@ -1,6 +1,12 @@
 Miniblog::Admin::PostsController.class_eval do
   def after_post_is_saved
-    @post.purge_all
+    if Rails.env.production? && @post.published?
+      @post.purge_all
+      @post.purge
+    else
+      Rails.logger.debug("Purge Cache: #{@post.table_key}")
+      Rails.logger.debug("Purge Cache: #{@post.record_key}")
+    end
   end
 
   def post_params
--- app/decorators/models/miniblog/posts_decorator.rb
+++ app/decorators/models/miniblog/posts_decorator.rb
@@ -8,15 +8,5 @@ Miniblog::Post.class_eval do
   def to_param
     "#{id}-#{permalink}"
   end
-
-  alias_method :purge_all!, :purge_all
-
-  def purge_all
-    if Rails.env.production?
-      purge_all!
-    else
-      Rails.logger.debug("Purge Cache: #{table_key}")
-    end
-  end
 end
 
