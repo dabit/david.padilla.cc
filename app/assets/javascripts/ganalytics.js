b4b95500ecf3683cadc4fc9bdda622b92bddae5b
David Padilla <david@easybroker.com>
2015-04-22 21:53:09 -0500

    Send analytics events

---
+++ app/assets/javascripts/ganalytics.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.archive-link a').bind('click', {'category': 'archive-link'}, sendAnalyticsEvent);
+  $('.more-link a').bind('click', {'category': 'more-link'}, sendAnalyticsEvent);
+
+  function sendAnalyticsEvent(e, category){
+    ga('send', 'event', e.data.category, 'click', $(e.currentTarget).text());
+  }
+});
